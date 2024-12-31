5004ac589e1f45d463074070b82c126ba6cb957e
yerakairzhan <erakairzhan@gmail.com>
2024-12-31 16:25:08 +0500

    first commit

--- migrations/000001_create_checks_table.up.sql
+++ migrations/000001_create_checks_table.up.sql
@@ -5,5 +5,6 @@ CREATE TABLE users (
       purchased Boolean default false,
       language_code VARCHAR(10) DEFAULT 'en' not null ,
       chosen_package varchar(255) default 'no' not null,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       UNIQUE (user_id, username)
 );
